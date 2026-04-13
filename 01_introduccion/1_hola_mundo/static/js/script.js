981ff9b70a8dd6d7d6a14ef16f79019da894b362
mitsu-gami00 <ignacioorellana@liceovvh.cl>
2026-04-13 14:08:58 -0400

    asd

---
+++ 01_introduccion/1_hola_mundo/static/js/script.js
@@ -0,0 +1,13 @@
+// prueba de conexion con js
+console.log("conexion exitosa con js externo...");
+
+// 1) hola mundo con variable y const.
+const saludo = "hola a todos. ";
+let destinatario = "estudiantes"
+// Impresion de saludo encantado con (+)
+console.log(saludo + "para los" + destinatario);
+
+// 2) saludar a una persona ingresada por teclado.
+let nombre = prompt("ingrese su nombre: ")
+//Imprimir un nombre ingresado con un prompt
+console.log("hola, + nombre");
---
+++ 02_tipos_de_datos/1.-tipo_textos/static/js/script.js
@@ -0,0 +1,10 @@
+console.log("conexion con js correcta...");
+/*
+dato: tipo texto(string)
+concatenacion de texto con el signo +
+podemos unir texto y variables
+/*
+const nombre = Javiera
+const apellido = leon
+//Unimos ambas constantes con un texto extra.
+console.log("hola, mi nombre es: " + nombre + " " + apellido)
